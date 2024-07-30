ff47a0e5cf497a3da1af2b0d916dbd68345e2168
limingyang <865713897@qq.com>
2024-07-30 14:26:18 +0800

    feat: 还原为webpack编译模式

--- packages/elza/src/server/server.ts
+++ packages/elza/src/server/server.ts
@@ -1,119 +1,122 @@
+import http from 'http';
 import express from 'express';
-import { resolve, join } from 'path';
-import { writeFileSync } from 'fs';
-import esbuild from 'esbuild';
-import { externalGlobalPlugin } from 'esbuild-plugin-external-global';
+import cors from 'cors';
+import webpack, { Stats, Configuration } from 'webpack';
+import WebpackDevMiddleware from 'webpack-dev-middleware';
+import { getDevBanner } from '../utils/getDevBanner';
+import logger from '../utils/logger';
+import { MESSAGE_TYPE } from '../constants';
+import { createWebSocketServer } from './ws';
+import { IConfig } from '../types';
 
 interface IOpts {
+  webpackConfig: Configuration;
   cwd: string;
+  userConfig: IConfig;
+  port?: number;
 }
 
-export async function createServer(opts: IOpts) {
-  const server = express();
+export const createServer = async (opts: IOpts) => {
+  const { webpackConfig, userConfig } = opts;
+  let ws: ReturnType<typeof createWebSocketServer>;
+  const app = express();
+  const compiler = webpack(webpackConfig);
 
-  await precompile(opts.cwd);
-  // 用于存储构建结果的变量
-  let buildResult: any = await esbuildServer(opts);
-
-  // 中间件，提供内存中的构建结果
-  server.use((req, res, next) => {
-    if (buildResult) {
-      const filePath = join('dist', req.path);
-      const file = buildResult.outputFiles.find((file: any) => file.path.endsWith(filePath));
-      if (file) {
-        res.set('Content-Type', getContentType(filePath));
-        res.send(Buffer.from(file.contents));
-        return;
-      }
-    }
-    next();
-  });
-
-  // server.use('/vender', express.static(resolve(opts.cwd, './node_modules/.elza/vender')));
-  server.get('/', (_req, res) => {
-    res.set('Content-Type', 'text/html');
-    res.send(`<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Test</title>
-    <script src="https://unpkg.com/react@17.0.2/umd/react.development.js"></script>
-  <script src="https://unpkg.com/react-dom@17.0.2/umd/react-dom.development.js"></script>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script defer src="/index.js"></script>
-  </body>
-</html>
-`);
-  });
-  server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
-}
-
-async function esbuildServer(opts: IOpts) {
-  const start = new Date().getTime();
-  const context = await esbuild.build({
-    entryPoints: [resolve(opts.cwd, 'src/index.jsx')],
-    bundle: true,
-    write: false,
-    platform: 'browser',
-    outdir: 'dist',
-    format: 'cjs',
-    external: ['react', 'react-dom'],
-    // banner: {
-    //   js: 'var require = createRequire(import.meta.url)',
-    // },
-    plugins: [
-      // externalGlobalPlugin({
-      //   react: 'window.React',
-      //   'react-dom': 'window.ReactDOM',
-      // }),
-    ],
-  });
-  console.log(new Date().getTime() - start, 'end');
-  // console.log(context, 'context');
-  return context;
-}
-
-async function precompile(cwd: string) {
-  const dependencies = [
-    'react',
-    // 'react-dom',
-    // 添加其他需要预编译的依赖包
-  ];
-  await esbuild
-    .build({
-      entryPoints: dependencies,
-      bundle: true,
-      minify: true,
-      sourcemap: true,
-      globalName: 'React',
-      format: 'iife',
-      platform: 'browser',
-      outdir: join(cwd, 'node_modules/.elza', 'vender'),
-    })
-    .catch(() => process.exit(1));
-
-  const moduleMap = dependencies.reduce((map, dep) => {
-    map[dep] = `${cwd}/node_modules/.elza/vender/${dep}.js`;
-    return map;
-  }, {});
-
-  writeFileSync(
-    join(cwd, 'node_modules/.elza', 'moduleMap.json'),
-    JSON.stringify(moduleMap, null, 2),
+  app.use(
+    cors({
+      origin: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'PATCH'],
+      credentials: true,
+    }),
   );
-}
 
-// 根据文件扩展名设置适当的内容类型
-function getContentType(filePath: string) {
-  if (filePath.endsWith('.js')) return 'application/javascript';
-  if (filePath.endsWith('.css')) return 'text/css';
-  if (filePath.endsWith('.html')) return 'text/html';
-  if (filePath.endsWith('.png')) return 'image/png';
-  if (filePath.endsWith('.svg')) return 'image/svg+xml';
-  return 'text/plain';
-}
+  app.use(
+    WebpackDevMiddleware(compiler, {
+      publicPath: userConfig.publicPath || '/',
+      writeToDisk: userConfig.writeToDisk,
+      stats: 'none',
+    }),
+  );
+
+  // hmr
+  let stats: any;
+  compiler.hooks.invalid.tap('server', () => {
+    sendMessage(MESSAGE_TYPE.invalid);
+  });
+  compiler.hooks.done.tap('server', (_stats: Stats) => {
+    stats = _stats;
+    sendStats(getStats(stats));
+  });
+
+  const sendMessage = (type: string, data?: any, sender?: any) => {
+    (sender || ws)?.send(JSON.stringify({ type, data }));
+  };
+
+  function sendStats(stats: webpack.StatsCompilation, force?: boolean, sender?: any) {
+    const shouldEmit =
+      !force &&
+      stats &&
+      (!stats.errors || stats.errors.length === 0) &&
+      (!stats.warnings || stats.warnings.length === 0) &&
+      stats.assets &&
+      stats.assets.every((asset) => !asset.emitted);
+    if (shouldEmit) {
+      sendMessage(MESSAGE_TYPE.stillOk, null, sender);
+      return;
+    }
+    sendMessage(MESSAGE_TYPE.hash, stats.hash, sender);
+    if (
+      (stats.errors && stats.errors.length > 0) ||
+      (stats.warnings && stats.warnings.length > 0)
+    ) {
+      if (stats.warnings && stats.warnings.length > 0) {
+        sendMessage(MESSAGE_TYPE.warnings, stats.warnings, sender);
+      }
+      if (stats.errors && stats.errors.length > 0) {
+        sendMessage(MESSAGE_TYPE.errors, stats.errors, sender);
+      }
+    } else {
+      sendMessage(MESSAGE_TYPE.ok, null, sender);
+    }
+  }
+
+  function getStats(stats: Stats) {
+    return stats.toJson({
+      all: false,
+      hash: true,
+      assets: true,
+      warnings: true,
+      errors: true,
+      errorDetails: false,
+    });
+  }
+
+  app.use('/__elza_ping', (_, res) => {
+    res.send('pong');
+  });
+
+  const server = http.createServer(app);
+
+  ws = createWebSocketServer(server);
+  ws.wss.on('connection', (socket) => {
+    if (stats) {
+      sendStats(getStats(stats), false, socket);
+    }
+  });
+
+  const port = opts.port || 3000;
+  server.listen(port, () => {
+    const { before, main, after } = getDevBanner();
+    console.log(before);
+    logger.ready(main);
+    console.log(after);
+  });
+  process.on('SIGINT', () => {
+    server.close();
+    process.exit(0);
+  })
+  process.on('SIGTERM', () => {
+    server.close();
+    process.exit(1);
+  })
+};
--- packages/elza/src/types.ts
+++ packages/elza/src/types.ts
@@ -1,3 +1,33 @@
+import { IAutoRoutes } from 'webpack-plugin-auto-routes';
+import Config from '../compiled/webpack-5-chain';
+
+export enum Env {
+  development = 'development',
+  production = 'production',
+}
+
 export interface IConfig {
-  cwd: string;
+  transpiler?: 'babel' | 'swc' | 'esbuild';
+  htmlTemplate?: string;
+  publicPath?: string;
+  devtool?: Config.DevTool;
+  writeToDisk?: boolean | ((targetPath: string) => boolean);
+  externals?: Record<string, string>;
+  alias?: Record<string, string>;
+  autoRoutes?: IAutoRoutes;
+  pluginOptions?: Record<string, any>;
+  chainWebpack?: (webpackConfig: any, args: { env: 'development' | 'production' }) => void;
+  [key: string]: any;
+}
+
+export enum Transpiler {
+  babel = 'babel',
+  swc = 'swc',
+  esbuild = 'esbuild',
+}
+
+export interface RGB {
+  r: number;
+  g: number;
+  b: number;
 }
